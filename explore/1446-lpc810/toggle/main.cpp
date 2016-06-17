dc68698f74799b8b10899965847162824138cf0a
Leonor Oliveira <leonorfmartins@gmail.com>
2016-06-17 15:38:31 +0200

    Make 3 lights blink sequencially for around 2 seconds

--- explore/1446-lpc810/toggle/main.cpp
+++ explore/1446-lpc810/toggle/main.cpp
@@ -3,16 +3,30 @@
 
 #include "LPC8xx.h"
 
+void wait() {
+    for (int count = 0; count < 2000000; ++count)
+	    __ASM("");                      // waste time while drawing current
+}
+
 int main () {
-    LPC_SWM->PINENABLE0 |= (1<<2); // disable SWCLK (makes pin 3 a GPIO pin)
-    LPC_GPIO_PORT->DIR0 |= (1<<3); // set PIO0_3 as output
+    //LPC_SWM->PINENABLE0 |= (1<<2); // disable SWCLK (makes pin 3 a GPIO pin)
+    LPC_GPIO_PORT->DIR0 |= 1+2+16; // set PIO0_0, _1 and _4 as outputs
+
 
     while (true) {
-		for (int count = 0; count < 2000000; ++count)
-			__ASM("");                      // waste time while drawing current
-		LPC_GPIO_PORT->B0[3] = 1;               // set GPIO 3 high (pin 3)
-		for (int count = 0; count < 2000000; ++count)
-			__ASM("");                      // waste time while drawing current
-		LPC_GPIO_PORT->B0[3] = 0;
+
+	    LPC_GPIO_PORT->B0[0] = 1;               // set GPIO 3 high (pin 3)
+	    wait();
+	    LPC_GPIO_PORT->B0[0] = 0;
+	    wait();
+	    LPC_GPIO_PORT->B0[1] = 1;               // set GPIO 3 high (pin 3)
+	    wait();
+	    LPC_GPIO_PORT->B0[1] = 0;               // set GPIO 3 high (pin 3)
+	    wait();
+	    LPC_GPIO_PORT->B0[4] = 1;               // set GPIO 3 high (pin 3)
+	    wait();
+	    LPC_GPIO_PORT->B0[4] = 0;               // set GPIO 3 high (pin 3)
+	    wait();
 	};
 }
+
